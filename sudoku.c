f49b55f8e0930141ce902c124846df376676f3fd
kimmycloud <yang.kimmy@gmail.com>
2026-03-28 01:36:38 -0400

    v9 - redesigned board printing, fast pencil mark generation

--- sudoku.c
+++ sudoku.c
@@ -1,28 +1,66 @@
 #include <stdio.h>
+#include <string.h>
+
 #define red "\x1b[31m"
-#define green "\x1b[32m"
+#define green "\x1b[32m" 
+#define purple "\x1b[35m"
 #define resetcolor "\x1b[0m"
+#define bold "\x1b[1m"
+#define italic "\x1b[3m"
+#define underline "\x1b[4m"
+#define background "\x1b[45m" // good ones: 40dark grey, purple 45, green 42
+
+// To print Unicode characters in Windows
+#ifdef _WIN32
+	#include <windows.h>
+#endif
+
+#define sub1 "\u2081"
+#define sub2 "\u2082"
+#define sub3 "\u2083"
+#define sub4 "\u2084"
+#define sub5 "\u2085"
+#define sub6 "\u2086"
+#define sub7 "\u2087"
+#define sub8 "\u2088"
+#define sub9 "\u2089"
 
 void print_3rows (int board[9][9], int startrow, int *highlighted); // prints the numbers on board, 3 rows at a time
-void print_board (int board[9][9], int *highlighted); // prints the entire sudoku board
+void print_boardpencil (int board[9][9], int pencil [3][3][3][3][3][3], int *highlighted);
+//void print_board (int board[9][9], int *highlighted); // prints the entire sudoku board
 void make3x3grid (int board[9][9], int grid [3][3][3][3]); // converts 9x9 grid into 3x3x3x3 grid
+int findrow (int board[9][9], int row, int entry);
+int findcolumn (int board[9][9], int column, int entry);
+int findblock (int grid [3][3][3][3], int row, int column, int entry);
 int validmove (int board[9][9], int grid [3][3][3][3], int row, int column, int entry); // checks same row, column or 3x3 grid
 int unfinishedboard (int board [9][9]); // checks if the board is incomplete
 struct Numbers; // declares a struct that counts tally of each number on board
 void numbers_tally (int board[9][9], struct Numbers *tallyptr); // counts how many of each number is on board
 int solvepuzzle (int solvedpuzzle[9][9], int solvedpuzzlegrid [3][3][3][3]); // uses recursion to make a solved puzzle to check user's mistakes
-void inputoutcome (int isvalid, int board[9][9], int solvedpuzzle[9][9], int row, int column, int entry, int *mistakes); // prints if move is valid
+void inputoutcome (int isvalid, int board[9][9], int solvedpuzzle[9][9], int row, int column, int entry, int *mistakes, char *message, int pencil [3][3][3][3][3][3]); // prints if move is valid
 void validateinput (int board[9][9], char rowchar[10], int *row, char columnchar[10], int *column, char entrychar[10], int *entry); // ensures the user inputs 0-9
-void highlight (void (*print_board)(int board[9][9], int *highlighted), int board [9][9], int *highlighted); // highlights all the same numbers
+void highlight (void (*print_board)(int board[9][9], int pencil [3][3][3][3][3][3], int *highlighted), int board [9][9], int pencil [3][3][3][3][3][3], int *highlighted); // highlights all the same numbers
+void updatepencil (int board[9][9], int pencil [3][3][3][3][3][3], int row, int column, int entry);
+int haspencilmarks (int pencil [3][3][3][3][3][3], int i, int j, int k, int l);
+void generatepencil (int board[9][9], int grid[3][3][3][3], int pencil [3][3][3][3][3][3]);
+void clear_screen();
+
 
 int main () {
+	// To print Unicode characters in Windows
+	#ifdef _WIN32
+		SetConsoleOutputCP(65001);
+	#endif
+	
 	FILE *in; // Declare file
 	in = fopen ("sudokutest2.txt", "r"); // Open file with test values in read mode
-	
 	int board [9][9], grid [3][3][3][3], solvedpuzzle [9][9], solvedpuzzlegrid [3][3][3][3], row, column, entry, option;
 	char rowchar[10], columnchar[10], entrychar[10], optionchar[10];
+	char message[100] = "";
 	int mistakes=0;
 	int highlighted = -1; // Don't want to highlight any number for default
+	int pencil[3][3][3][3][3][3];
+	
 	// Scan file for values to make the Sudoku board
 	for (int i = 0; i<9; i++) {
 		for (int j = 0 ; j < 9; j++) {
@@ -30,14 +68,21 @@ int main () {
 			solvedpuzzle[i][j] = board[i][j]; // Put a copy of board into solvedpuzzle
 		}
 	}
+	
 	make3x3grid(solvedpuzzle, solvedpuzzlegrid); // Make a 3x3x3x3 version of 9x9 solvedpuzzle
+	make3x3grid(board, grid);
+	generatepencil(board, grid, pencil);
 	solvepuzzle(solvedpuzzle, solvedpuzzlegrid); // Store the solved puzzle in solvedpuzzle
+	
 	do {
+		clear_screen();
 		printf("\n");
-		print_board(board, &highlighted); // Print the sudoku board
+		print_boardpencil(board, pencil, &highlighted);
 		printf("\n");
+		printf("%s\n", message);
+		message[0] = '\0'; // erase the message after printing
 		make3x3grid(board, grid); // Make a 3x3x3x3 version of the 9x9 board
-		
+		generatepencil(board, grid, pencil);
 		// Keep prompting user to pick an option as long as they don't input 1 or 2
 		do {
 			printf("Choose an option:\n(1) Highlight a number\n(2) Fill a cell\n"); 
@@ -45,21 +90,33 @@ int main () {
 			option = optionchar[0] - '0'; // Convert char to int
 		} while (optionchar[1] != '\0' || (optionchar[0] != '1' && optionchar[0] != '2'));
 		
-		if (option ==1) highlight(print_board, board, &highlighted); // Option 1: highlight a number
+		if (option ==1) highlight(print_boardpencil, board, pencil, &highlighted); // Option 1: highlight a number
 		// Option 2: take user input for cell
 		else if (option == 2) {
 			validateinput(board, rowchar, &row, columnchar, &column, entrychar, &entry); // ask user for input, validate each input
 			printf("\n");
 			int isvalid = validmove(board, grid, row, column, entry); // check if the move is valid
-			inputoutcome(isvalid, board, solvedpuzzle, row, column, entry, &mistakes); // based on isvalid, print invalid or fill cell
+			inputoutcome(isvalid, board, solvedpuzzle, row, column, entry, &mistakes, message, pencil); // based on isvalid, print invalid or fill cell
 		}	
+		
 	} while (unfinishedboard (board)); // keep repeating until the board no longer has 0
-	highlighted = -1;
-	print_board(board, &highlighted); // print the final finished board
+	
+	highlighted = -1; // Set highlighted back to -1 when printing the finished board
+	clear_screen();
+	print_boardpencil(board, pencil, &highlighted); // print the final finished board
 	printf(green"CONGRATULATIONS! Sudoku puzzle solved!\n"resetcolor);
 	
 	fclose(in); // Close file
 	return 0;
+}
+
+void clear_screen() {
+	#ifdef _WIN32
+		//#include <windows.h> (already included above)
+		system("cls");
+	#else
+		printf("\033[2J\033[H");
+	#endif	
 }
 
 void validateinput (int board[9][9], char rowchar[10], int *row, char columnchar[10], int *column, char entrychar[10], int *entry) {
@@ -89,33 +146,6 @@ void validateinput (int board[9][9], char rowchar[10], int *row, char columnchar
 		*entry = entrychar[0] - '0';
 	} while (entrychar[1] != '\0' || entrychar[0] <'1' || entrychar[0] > '9');
 }
-// print_3rows function prints three rows at a time
-void print_3rows (int board [9][9], int startrow, int *highlighted) {
-	// Parameter startrow allows us to call print_3rows
-	// starting from different initial values
-	
-	for (int i = startrow; i < startrow + 3; i++) {
-		for (int j = 0; j < 9; j++) {
-			// Print value first
-			if (board [i][j] == 0) printf(".");
-			else if (board[i][j] == *highlighted) printf(green"%d"resetcolor, board[i][j]);
-			else printf("%d", board[i][j]);
-			
-			// Except for last column, separate values by spaces
-			// Separate every three values by a separator |
-			if (j != 8) {
-				((j+1) % 3 ==0) ? printf("  |  ") : printf("   ");
-			}
-		}
-		// Except for rows that are multiples of 3, print separator lines
-		// after each row (the ones that separate 3x3 grids)
-		printf("\n");
-		if ((i+1)%3 != 0){
-			printf("           |");
-			printf("             |\n");
-		}
-	}
-}
 
 struct Numbers {
 	int ones;
@@ -128,6 +158,7 @@ struct Numbers {
 	int eights;
 	int nines;
 };
+
 // this function needs to be seen by print_board function
 void numbers_tally (int board[9][9], struct Numbers *tallyptr) {
 	// For each value in board, check how many of each number there are
@@ -146,47 +177,75 @@ void numbers_tally (int board[9][9], struct Numbers *tallyptr) {
 	}
 }
 
-void print_board (int board [9][9], int *highlighted) {
-	// Use for loop to call print_3rows (for loop increment by 3)
-	for (int i =0; i<=6; i+=3) {
-		print_3rows(board, i, highlighted); // print three rows at a time
-		
-		// If not the last set of triples, print separator rows
-		if (i !=6) {
-			for (int j = 0; j < 38; j++) {
-				printf("_");
-			}
-			printf("\n\n");
+void print_boardpencil (int board[9][9], int pencil [3][3][3][3][3][3], int *highlighted) {
+	for (int x=0; x<9; x++) { // board row
+		for (int m = 0; m<3; m++) { // pencil mark row
+			printf("   ");
+			for (int y = 0; y <9; y++) { // board column
+				int i = (int)((x+1)/3.0 - 0.01); // convert 9x9 row to 3x3 block row
+				int j = (int) ((y+1)/3.0 - 0.01); // convert 9x9 column to 3x3 block column
+				int k = x % 3; // convert 9x9 row to cell row within 3x3 block
+				int l = y % 3; // convert 9x9 column to cell column within 3x3 block
+				for (int n = 0; n<3; n++) { // pencil mark column
+					int mark = m*3 + 1 +n; // convert m and n into the value of the pencil mark
+					char *subs[10] = {"", sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9};
+					if (board[x][y] !=0 && board[x][y] == *highlighted && m==1 && n==1) {
+						printf(background bold"%d"resetcolor, board[x][y]);  // print board value in centre of mxn matrix
+					} else if (board[x][y] !=0 && board[x][y] != *highlighted && m==1 && n==1) {
+						printf(bold "%d"resetcolor, board[x][y]);  // print board value in centre of mxn matrix
+					} else if (board[x][y] !=0 && board[x][y] == *highlighted) {
+						printf(background " " resetcolor);
+					} else if (board[x][y]==0 && pencil[i][j][k][l][m][n] && mark == *highlighted) {
+						printf(bold background"%s"resetcolor, subs[mark]); // print the pencil mark in its respective coordinates within pencil matrix
+					} else if (board[x][y]==0 && pencil[i][j][k][l][m][n] && mark != *highlighted) {
+						printf("%s", subs[mark]); // print the pencil mark in its respective coordinates within pencil matrix
+					} else if (board [x][y] == 0 && m==1 && n==1 && !haspencilmarks(pencil, i, j, k, l)) {
+						printf(".");
+					} else {
+						printf(" ");
+					}	
+				}
+				if (y==2 || y ==5) printf("    |    "); // vertical lines to separate the 3x3 blocks
+				else if (y != 8) printf("     "); // add space between each number
+			} 
+			printf("\n");	
+		}
+		printf("                          |                           |\n");
+		if (x==2 || x==5) {
+				for (int s=0; s<79;s++) printf("_");
+				printf("\n\n");
 		}
 	}
 	
-	// Print remaining 1-9 under the sudoku board
-	struct Numbers tally = {0,0,0,0,0,0,0,0,0};
-	numbers_tally(board, &tally);
-	
-	printf("\nRemaining to fill:\n\n");
-	
-	if (tally.ones == 9) printf("   ");
-		else printf("  1");
-	if (tally.twos == 9) printf("    ");
-		else printf("   2");
-	if (tally.threes == 9) printf("    ");
-		else printf("   3");
-	if (tally.fours == 9) printf("    ");
-		else printf("   4");
-	if (tally.fives == 9) printf("    ");
-		else printf("   5");
-	if (tally.sixes == 9) printf("    ");
-		else printf("   6");
-	if (tally.sevens == 9) printf("    ");
-		else printf("   7");
-	if (tally.eights == 9) printf("    ");
-		else printf("   8");
-	if (tally.nines == 9) printf("      \n");
-		else printf("   9  \n");
+	if (unfinishedboard (board)) {
+		// Print remaining 1-9 under the sudoku board
+		struct Numbers tally = {0,0,0,0,0,0,0,0,0};
+		numbers_tally(board, &tally);
+		printf(bold"\n                               Remaining to fill:\n\n"resetcolor);
+		
+		if (tally.ones == 9) printf("             ");
+			else printf(bold"            1"resetcolor);
+		if (tally.twos == 9) printf("       ");
+			else printf(bold"      2"resetcolor);
+		if (tally.threes == 9) printf("       ");
+			else printf(bold"      3"resetcolor);
+		if (tally.fours == 9) printf("       ");
+			else printf(bold"      4"resetcolor);
+		if (tally.fives == 9) printf("       ");
+			else printf(bold"      5"resetcolor);
+		if (tally.sixes == 9) printf("       ");
+			else printf(bold"      6"resetcolor);
+		if (tally.sevens == 9) printf("       ");
+			else printf(bold"      7"resetcolor);
+		if (tally.eights == 9) printf("       ");
+			else printf(bold"      8"resetcolor);
+		if (tally.nines == 9) printf("         \n");
+			else printf(bold"      9  \n"resetcolor);
+	}
 }
 
-void highlight (void (*print_board)(int board[9][9], int *highlighted), int board [9][9], int *highlighted) {
+
+void highlight (void (*print_boardpencil)(int board[9][9], int pencil [3][3][3][3][3][3], int *highlighted), int board [9][9], int pencil [3][3][3][3][3][3], int *highlighted) {
 	char highlightchar[10];
 	do {	
 			printf("Pick a number to highlight (1-9): ");
@@ -209,24 +268,63 @@ void make3x3grid (int board[9][9], int grid [3][3][3][3]) {
 	}
 }
 
-int validmove (int board[9][9], int grid [3][3][3][3], int row, int column, int entry) {
-	// Check if entry is already in that row
+int findrow (int board[9][9], int row, int entry) {
 	for (int i = 0; i < 9; i++) {
-		if (board[row][i] == entry) return 0;
-	}
-	// Check if entry is already in that column
+		if (board[row][i] == entry) return i;
+	} return -1;
+}
+
+int findcolumn (int board[9][9], int column, int entry) {
 	for (int i = 0; i < 9; i++) {
-		if (board[i][column] == entry) return -1;
-	}
-	// Check if entry is already in that 9x9 box
+		if (board[i][column] == entry) return i;
+	} return -1;
+}
+
+int findblock (int grid [3][3][3][3], int row, int column, int entry) {
 	int i = (int) ((row+1)/3.0 -0.01);
-	int j = (int) ((column+1)/3.0 -0.01);
+	int j = (int) ((column +1)/3.0 -0.01);
 	for (int k =0; k <3; k++) {
 		for (int l = 0; l<3; l++) {
-			if (grid [i][j][k][l] == entry) return -2;
+			if (grid [i][j][k][l] == entry) return 3*k + l; // index if the cells in the ixj matrix were numbered 0-8
 		}
-	}
+	} return -1;
+}
+
+int validmove (int board[9][9], int grid [3][3][3][3], int row, int column, int entry) {
+	if (findrow(board, row, entry) != -1) return 0;
+	if (findcolumn(board, column, entry) != -1) return -1;
+	if (findblock(grid, row, column, entry) != -1) return -2;
 	return 1;
+}
+
+void updatepencil (int board[9][9], int pencil [3][3][3][3][3][3], int row, int column, int entry) {
+	int m = (int) ((entry -1.0) / 3.0);
+	int n = (int) (entry - 1.0) % 3;
+	
+	for (int y=0; y<9; y++) {
+		// Same row
+		int i = row/3;
+		int j = y/3;
+		int k = row % 3;
+		int l = y % 3;
+		
+		if (y != column) pencil[i][j][k][l][m][n] = 0;
+		
+		// Same column
+		i = y/3;
+		j = column/3;
+		k = y % 3;
+		l = column % 3;
+		if (y != row) pencil[i][j][k][l][m][n] = 0;
+	}
+	
+	// Check if same block
+	int i = row/3;
+	int j = column/3;
+	for (int k=0; k<3; k++) 
+		for (int l=0; l<3; l++) 
+			if ((i*3 + k) != row || (j*3 + l) != column) 
+				pencil[i][j][k][l][m][n] = 0;
 }
 
 int unfinishedboard (int board [9][9]) {
@@ -262,26 +360,59 @@ int solvepuzzle (int solvedpuzzle[9][9], int solvedpuzzlegrid [3][3][3][3]) {
 	return 1;
 }
 
-void inputoutcome (int isvalid, int board[9][9], int solvedpuzzle[9][9], int row, int column, int entry, int *mistakes) {
-	if (isvalid ==0) {
-		(entry == 8) ? printf(red"There is already an %d in this row. Please try again.\n"resetcolor, entry) : 
-		printf(red"There is already a %d in this row. Please try again.\n"resetcolor, entry);
-	} else if (isvalid == -1) {
-		(entry ==8) ? printf(red"There is already an %d in this column. Please try again.\n"resetcolor, entry) :
-		printf(red"There is already a %d in this column. Please try again.\n"resetcolor, entry);
-	} else if (isvalid == -2) {
-		(entry ==8) ? printf(red"There is already an %d in this 3x3 block. Please try again.\n"resetcolor, entry) :
-		printf(red"There is already a %d in this 3x3 block. Please try again.\n"resetcolor, entry);
-	} else if (isvalid==1) {
-		if (entry != solvedpuzzle[row][column]) {
-			(*mistakes)++;
-			printf(red"Wrong number inputted. Mistakes: %d\n"resetcolor, *mistakes);
-		} else {
-			board[row][column] = entry;
-			printf(green"Nice! Grid is now updated with your input\n"resetcolor);
+int haspencilmarks (int pencil [3][3][3][3][3][3], int i, int j, int k, int l) {
+	for (int m = 0; m<3; m++)
+		for (int n=0; n<3; n++)
+			if (pencil[i][j][k][l][m][n]) return 1;
+	return 0;
+}
+
+void generatepencil (int board[9][9], int grid[3][3][3][3], int pencil [3][3][3][3][3][3]) {
+	for (int x = 0; x<9; x++) {
+		for (int y = 0; y<9; y++) {
+			if (board[x][y] == 0) {
+				int i = x/3;
+				int j = y/3;
+				int k = x % 3;
+				int l = y % 3;
+				for (int mark = 1; mark <=9; mark++) {
+					int m = (mark - 1)/3;
+					int n = (mark - 1) % 3;
+					if (validmove(board, grid, x, y, mark) == 1) {
+						pencil[i][j][k][l][m][n] = 1;
+					} else {
+						pencil[i][j][k][l][m][n] = 0;
+					}
+				}
+			}
 		}
 	}
 }
+
+
+void inputoutcome (int isvalid, int board[9][9], int solvedpuzzle[9][9], int row, int column, int entry, int *mistakes, char *message, int pencil [3][3][3][3][3][3]) {
+	if (isvalid ==0) {
+		(entry == 8) ? sprintf(message,bold italic red"There is already an %d in this row. Please try again.\n"resetcolor, entry) : 
+		sprintf(message, bold italic red"There is already a %d in this row. Please try again.\n"resetcolor, entry);
+	} else if (isvalid == -1) {
+		(entry ==8) ? sprintf(message, bold italic red"There is already an %d in this column. Please try again.\n"resetcolor, entry) :
+		sprintf(message, bold italic red"There is already a %d in this column. Please try again.\n"resetcolor, entry);
+	} else if (isvalid == -2) {
+		(entry ==8) ? sprintf(message, bold italic red"There is already an %d in this 3x3 block. Please try again.\n"resetcolor, entry) :
+		sprintf(message, bold italic red"There is already a %d in this 3x3 block. Please try again.\n"resetcolor, entry);
+	} else if (isvalid==1) {
+		if (entry != solvedpuzzle[row][column]) {
+			(*mistakes)++;
+			sprintf(message, bold italic red"Wrong number inputted. Mistakes: %d\n"resetcolor, *mistakes);
+		} else {
+			board[row][column] = entry;
+			updatepencil (board, pencil, row, column, entry);
+			sprintf(message, bold italic green"Nice! Grid is now updated with your input\n"resetcolor);
+		}
+	}
+}
+
+
 
 	/* test print 3x3
 	 * test print the 3x3 grid
@@ -305,3 +436,13 @@ void inputoutcome (int isvalid, int board[9][9], int solvedpuzzle[9][9], int row
 		} printf("\n");
 	}
 	*/
+
+	// SCAN PENCIL FILE
+	/*for (int i = 0; i<3; i++) 
+		for (int j = 0; j<3; j++) 
+			for (int k = 0; k<3; k++) 
+				for (int l = 0; l<3; l++) 
+					for (int m = 0; m<3; m++) 
+						for (int n =0; n<3; n++) 
+							fscanf(pencilfile, "%d", &pencil[i][j][k][l][m][n]);
+*/
